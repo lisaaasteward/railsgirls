4071c0177755ec31ca1c6c37feb20b68c6c47c43
Lisa Steward <lisa.b.steward@gmail.com>
2014-03-16 15:04:54 -0700

    first commit

---
+++ vendor/cache/ruby/2.0.0/gems/atomic-1.1.15/ext/siteconf20140316-4508-1ueemp6.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140316-4508-1miialp"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
